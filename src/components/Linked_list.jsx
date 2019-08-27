1d74dc93d8da48007919bf10ea90f24bf7cc1f57
Domingo Cabeza <domingoacd1@gmail.com>
2019-08-27 10:35:38 -0400

    Added list state

--- src/components/Linked_list.jsx
+++ src/components/Linked_list.jsx
@@ -4,12 +4,40 @@ export default class Linked_list extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      list: {
+        key: 0,
+        data: "12",
+        next: {
+          data: "02",
+          next: {
+            data: "35",
+            next: null
+          }
+        }
+      }
+    }
+  }
+
+  getListElements() {
+    const elements = this.state.list;
+    let currentElement = elements;
+    let htmlElements = [];
+    while (currentElement !== null) {
+      const html = <div>{currentElement.data}</div>
+      htmlElements.push(html);
+      currentElement = currentElement.next;
+    }
+    return htmlElements;
   }
 
   render() {
     return (
       <div className="linked_list">
         <h1>Linked list</h1>
+        <div className="list-container">
+          {this.getListElements()}
+        </div>
       </div>
     );
   }
